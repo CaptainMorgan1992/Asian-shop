4717fc6589d964f60d35c10d52dd045411ea5aec
liina <liina.oliin@gmail.com>
2024-07-04 10:26:06 +0200

    fixed bugg with dependency

--- backend/src/main/java/com/example/backend/Repository/CategoryRepository.java
+++ backend/src/main/java/com/example/backend/Repository/CategoryRepository.java
@@ -1,13 +1,22 @@
 package com.example.backend.Repository;
 
 import com.example.backend.Entities.Category;
+import com.example.backend.Entities.Products;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, String> {
 
     @Query(value = "SELECT CASE WHEN COUNT(*) > 0 THEN TRUE ELSE FALSE END FROM categories WHERE category_name = :categoryName", nativeQuery = true)
     boolean findByName(String categoryName);
+
+    @Transactional
+    @Query("SELECT p FROM Products p WHERE p.category.id = :categoryId")
+    List<Products> fetchProductsByCategory(@Param("categoryId") int categoryId);
 }
