2b3b85aff7030e21d8596c1e4e5d4c86d2dad051
CaptainMorgan1992 <kimberly.morgan1992@outlook.com>
2024-08-13 15:33:08 +0200

    Changed productMapper

---
+++ frontend/src/components/Dropdown.jsx
@@ -0,0 +1,21 @@
+import {useState} from "react";
+
+export default function Dropdown({ productId, onAmountChange }) {
+    const [localAmount, setLocalAmount] = useState(1);
+
+    const handleAmountChange = (e) => {
+        const selectedAmount = e.target.value;
+        setLocalAmount(selectedAmount);
+        onAmountChange(productId, selectedAmount); // Pass this to the parent
+    };
+
+    return (
+        <div id={"dropdownDiv"}>
+            <select value={localAmount} onChange={handleAmountChange}>
+                {[...Array(10).keys()].map(i => (
+                    <option key={i + 1} value={i + 1}>{i + 1}</option>
+                ))}
+            </select>
+        </div>
+    );
+}
